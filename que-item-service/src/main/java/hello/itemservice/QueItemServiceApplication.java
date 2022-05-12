0058e109caf40d107e3e68d7367e226097aa25bb
quedevel <kdh90817@naver.com>
2022-05-12 22:50:00 +0900

    스프링 MVC - 웹 페이지 만들기 init

---
+++ que-item-service/src/main/java/hello/itemservice/QueItemServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueItemServiceApplication.class, args);
+    }
+
+}
