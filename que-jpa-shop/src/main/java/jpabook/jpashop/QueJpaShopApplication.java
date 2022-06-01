cf76cd3e2e1d890b062e9ba47cca29837d5613a0
quedevel <kdh90817@naver.com>
2022-06-01 15:45:57 +0900

    [스프링부트-JPA-활용-1] 프로젝트 생성

---
+++ que-jpa-shop/src/main/java/jpabook/jpashop/QueJpaShopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueJpaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueJpaShopApplication.class, args);
+    }
+
+}
