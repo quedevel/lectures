58b112d22532f9cd19119cb95f062876aa23a7b9
quedevel <kdh90817@naver.com>
2022-06-04 21:16:07 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- que-jpa-shop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ que-jpa-shop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne (ManyToOne, OneToOne)
@@ -26,5 +31,29 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDTO> ordersV2(){
+        return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(SimpleOrderDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDTO {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus status;
+        private Address address;
+
+        public SimpleOrderDTO(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            status = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
