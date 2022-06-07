dd77aa8b5f57e070b7ed43cef1a37813055bda2a
quedevel <kdh90817@naver.com>
2022-06-07 10:34:37 +0900

    실전! 스프링 데이터 JPA

---
+++ que-data-jpa/src/main/java/study/datajpa/QueDataJpaApplication.java
@@ -0,0 +1,13 @@
+package study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueDataJpaApplication.class, args);
+    }
+
+}
