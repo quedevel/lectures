0275f94f83bd119e8736562c9fc3c5edb4211616
quedevel <kdh90817@naver.com>
2022-06-08 17:35:34 +0900

    Web 확장 - 페이징과 정렬

--- que-data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ que-data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,13 +1,18 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDTO;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
 import javax.annotation.PostConstruct;
+import java.util.stream.IntStream;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,8 +30,22 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDTO> list(@PageableDefault(size = 5, sort = "username") Pageable pageable){
+        Page<MemberDTO> toMap = getMemberDTOS(pageable);
+        return toMap;
+    }
+
+    private Page<MemberDTO> getMemberDTOS(Pageable pageable) {
+        Page<MemberDTO> toMap = memberRepository.findAll(pageable)
+                .map(MemberDTO::new);
+        return toMap;
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        IntStream.range(1, 100).forEach(i -> {
+            memberRepository.save(new Member("user"+i, i));
+        });
     }
 }
