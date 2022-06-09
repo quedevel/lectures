568bf9173919f0045316ea27bdbb8451920895c1
quedevel <kdh90817@naver.com>
2022-06-09 16:20:24 +0900

    네이티브 쿼리

--- que-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ que-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -347,4 +347,28 @@ class MemberRepositoryTest {
         }
 
     }
+
+    @Test
+    void nativeQuery() throws Exception {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", teamA);
+        Member m2 = new Member("m2", teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        //when
+        Page<MemberProjection> page = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = page.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+            System.out.println("memberProjection.getTeamName() = " + memberProjection.getTeamName());
+        }
+
+        //then
+
+    }
+
 }
