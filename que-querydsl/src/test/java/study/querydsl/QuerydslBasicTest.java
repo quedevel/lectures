684836fe6639854713b007b56e9950bd33194777
quedevel <kdh90817@naver.com>
2022-06-12 13:58:50 +0900

    프로젝션과 결과 반환 - @QueryProjection

--- que-querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ que-querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -546,6 +547,18 @@ public class QuerydslBasicTest {
 
         for (UserDto userDto : fetch) {
             System.out.println("userDto = " + userDto);
+        }
+    }
+
+    @Test
+    void findDtoByQueryProjection() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
         }
     }
 }
