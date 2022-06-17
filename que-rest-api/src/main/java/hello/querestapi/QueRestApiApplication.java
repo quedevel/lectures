e4affc373ee594b5dfcfa3c98daa08623c8118ad
quedevel <kdh90817@naver.com>
2022-06-17 14:39:19 +0900

    que-rest-api init

---
+++ que-rest-api/src/main/java/hello/querestapi/QueRestApiApplication.java
@@ -0,0 +1,13 @@
+package hello.querestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueRestApiApplication.class, args);
+	}
+
+}
---
+++ que-rest-api/src/test/java/hello/querestapi/QueRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.querestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
