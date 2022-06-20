7b3e56567a18efc3261fd71ffe29b45d0060cf14
quedevel <kdh90817@naver.com>
2022-06-20 14:42:54 +0900

    que-rest-api Spring Security 적용

---
+++ que-rest-api/src/test/java/hello/querestapi/accounts/AccountServiceTest.java
@@ -0,0 +1,43 @@
+package hello.querestapi.accounts;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+@SpringBootTest
+@Transactional
+class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    void findByUsername() {
+        //given
+        Account actual = Account.builder()
+                .email("quedevel@naver.com")
+                .password("quedevel")
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        accountRepository.save(actual);
+
+        //when
+        UserDetailsService userDetailsService = this.accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername("quedevel@naver.com");
+
+        //then
+        assertThat(userDetails.getPassword()).isEqualTo(actual.getPassword());
+
+    }
+}
