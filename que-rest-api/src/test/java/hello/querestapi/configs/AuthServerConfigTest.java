d7c84c108ca73df6a6035bb166eab0546ddc881d
quedevel <kdh90817@naver.com>
2022-06-21 10:02:12 +0900

    que-rest-api Spring Security OAuth2 인증 서버 설정

---
+++ que-rest-api/src/test/java/hello/querestapi/configs/AuthServerConfigTest.java
@@ -0,0 +1,62 @@
+package hello.querestapi.configs;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import hello.querestapi.accounts.Account;
+import hello.querestapi.accounts.AccountRole;
+import hello.querestapi.accounts.AccountService;
+import hello.querestapi.common.BaseControllerTest;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+@SpringBootTest
+@Transactional
+class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @DisplayName("인증 토큰을 발급 받는 테스트")
+    void getAuthToken() throws Exception {
+        //given
+        String username = "quedevel@email.com";
+        String password = "quedevel";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+
+        accountService.saveAccount(account);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        //when
+        ResultActions actions = mockMvc.perform(post("/oauth/token")
+                        .with(httpBasic(clientId, clientSecret))
+                        .param("username", username)
+                        .param("password", password)
+                        .param("grant_type", "password"))
+                .andDo(print());
+
+        //then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
+
+    }
+
+}
