0ac0320f6964e72ee7c5f89badc9048098b832b3
quedevel <kdh90817@naver.com>
2023-01-08 14:17:56 +0900

    feat : using springboot api

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,14 +1,29 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+//@JsonIgnoreProperties(value = {"password"})
+@JsonFilter("UserInfo")
 public class User {
     private Integer id;
+
+    @Size(min = 2, message = "이름은 2글자 이상 입력해주세요.")
     private String name;
+    @Past
     private Date joinDate;
+
+    private String password;
+    private String ssn;
 }
