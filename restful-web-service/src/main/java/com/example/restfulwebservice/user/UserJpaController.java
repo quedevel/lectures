ac1e3694f0c2e133af422a5b4af5c71cf1a7f419
quedevel <kdh90817@naver.com>
2023-01-10 00:08:33 +0900

    feat: using Java Persistence API

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -0,0 +1,42 @@
+package com.example.restfulwebservice.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/jpa")
+public class UserJpaController {
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isEmpty()){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        EntityModel<User> entityModel = EntityModel.of(user.get(), linkTo.withRel("all-users"));
+
+        return entityModel;
+    }
+}
