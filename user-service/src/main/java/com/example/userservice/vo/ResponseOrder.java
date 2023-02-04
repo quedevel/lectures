f47a2d4e0bb855b99d3febef9d19eacd9c8bb22a
quedevel <kdh90817@naver.com>
2023-02-04 20:52:27 +0900

    feat: user microservice 기능 완료

---
+++ user-service/src/main/java/com/example/userservice/vo/ResponseOrder.java
@@ -0,0 +1,14 @@
+package com.example.userservice.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ResponseOrder {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+    private Date createdAt;
+    private String orderId;
+}
