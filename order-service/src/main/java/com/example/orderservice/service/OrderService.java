5cd3fbc0c1d2ce5a15ada83d6e05283e11a2dfd0
quedevel <kdh90817@naver.com>
2023-02-05 15:22:24 +0900

    feat: order service 추가

---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.jpa.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public Iterable<OrderEntity> getOrdersByUserId(String userId){
+        return orderRepository.findByUserId(userId);
+    }
+
+    public OrderDto getOrderByOrderId(String orderId){
+        OrderEntity orderEntity = orderRepository.findByOrderId(orderId);
+        OrderDto orderDto = new ModelMapper().map(orderEntity, OrderDto.class);
+        return orderDto;
+    }
+
+    public OrderDto createOrder(OrderDto orderDto){
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDto.getQty() * orderDto.getUnitPrice());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        OrderEntity orderEntity = mapper.map(orderDto, OrderEntity.class);
+        orderRepository.save(orderEntity);
+        OrderDto returnValue = mapper.map(orderEntity, OrderDto.class);
+
+        return returnValue;
+    }
+}
